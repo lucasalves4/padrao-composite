5ac4650d2abdee4953991873cdebce6fae196072
lucasalves4 <lucasalves.12@live.com>
2024-07-15 22:21:21 -0300

    first commit

---
+++ src/main/java/org/example/Secao.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subtarefa extends Conteudo {
+
+    private List<Conteudo> conteudos;
+
+    public Subtarefa(String descricao) {
+        super(descricao);
+        this.conteudos = new ArrayList<Conteudo>();
+    }
+
+    public void addConteudo(Conteudo conteudo) {
+        this.conteudos.add(conteudo);
+    }
+
+    public String getConteudo() {
+        String saida = "";
+        saida = "Subtarefa: " + this.getDescricao() + "\n";
+        for (Conteudo conteudo : conteudos) {
+            saida += conteudo.getConteudo();
+        }
+        return saida;
+    }
+}
